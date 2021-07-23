03209811848337b1976601c2308b7331f08bb670
LucasMull <muller.lucas@Hotmail.com>
2021-07-23 20:30:37 -0300

    feat: update imprimeTabela.c

--- Exercicio5/imprimeTabela.c
+++ Exercicio5/imprimeTabela.c
@@ -22,9 +22,10 @@ const char DP_FIELD2[] = "AVX DP MFLOP/s";
 
 // valores extraídos a partir dos campos
 struct val {       
-  double d[16];        // armazena valores
-  size_t n;            // qtd de valores
-  double runtime;      // tempo de execução
+  double d[16];          // armazena valores
+  size_t n;              // qtd de valores
+  double runtime_normal; // tempo de execução
+  double runtime_otimiz; // tempo de execução
 };
 
 // para qsort()
@@ -81,10 +82,12 @@ int main(void)
   char **fnames = getFiles(DIR_PATH, ".txt", &n_files); // extrair arquivos .txt
   assert(fnames != NULL && "Não foi possível receber 'fnames'");
 
-  struct val L3_64={0}, L3_100={0}, L3_128={0}, L3_2000={0}, L3_2048={0};
-  struct val L2_64={0}, L2_100={0}, L2_128={0}, L2_2000={0}, L2_2048={0};
-  struct val DP_64={0}, DP_100={0}, DP_128={0}, DP_2000={0}, DP_2048={0};
-  struct val AVX_64={0}, AVX_100={0}, AVX_128={0}, AVX_2000={0}, AVX_2048={0};
+  struct val L3_64={0}, L3_100={0}, L3_128={0}, L3_1000={0}, L3_1024={0}, L3_2000={0}, L3_2048={0}, L3_3000={0}, L3_4096={0}, L3_5000={0};
+  struct val L2_64={0}, L2_100={0}, L2_128={0}, L2_1000={0}, L2_1024={0},  L2_2000={0}, L2_2048={0}, L2_3000={0}, L2_4096={0}, L2_5000={0};
+  struct val DP_64={0}, DP_100={0}, DP_128={0}, DP_1000={0}, DP_1024={0}, DP_2000={0}, DP_2048={0}, DP_3000={0}, DP_4096={0}, DP_5000={0};
+  struct val AVX_64={0}, AVX_100={0}, AVX_128={0}, AVX_1000={0}, AVX_1024={0}, AVX_2000={0}, AVX_2048={0}, AVX_3000={0}, AVX_4096={0}, AVX_5000={0};
+
+  enum { OPTIMIZED, NORMAL } tag;
 
   // extrai informações dos arquivos
   FILE *f;
@@ -103,41 +106,57 @@ int main(void)
 
     const char *field;
     int offset=0;
+    long size;
     if (NULL != (aux = strstr(fnames[i], L3_FILE_PREFIX))) {
-      switch (strtol(aux+sizeof(L3_FILE_PREFIX), NULL, 10)) {
+      switch (size = strtol(aux+sizeof(L3_FILE_PREFIX), NULL, 10)) {
       case 64:   data = &L3_64;   break;
       case 100:  data = &L3_100;  break;
       case 128:  data = &L3_128;  break;
+      case 1000: data = &L3_1000; break;
+      case 1024: data = &L3_1024; break;
       case 2000: data = &L3_2000; break;
       case 2048: data = &L3_2048; break;
-      default:   exit(EXIT_FAILURE);
+      case 3000: data = &L3_3000; break;
+      case 4096: data = &L3_4096; break;
+      case 5000: data = &L3_5000; break;
+      default:   fprintf(stderr, "L3 - %ld", size); exit(EXIT_FAILURE);
       }
       field = L3_FIELD;
       offset = sizeof(L3_FIELD);
     }
     else if (NULL != (aux = strstr(fnames[i], L2_FILE_PREFIX))) {
-      switch (strtol(aux+sizeof(L2_FILE_PREFIX), NULL, 10)) {
+      switch (size = strtol(aux+sizeof(L2_FILE_PREFIX), NULL, 10)) {
       case 64:   data = &L2_64;   break;
       case 100:  data = &L2_100;  break;
       case 128:  data = &L2_128;  break;
+      case 1000: data = &L2_1000; break;
+      case 1024: data = &L2_1024; break;
       case 2000: data = &L2_2000; break;
       case 2048: data = &L2_2048; break;
-      default:   exit(EXIT_FAILURE);
+      case 3000: data = &L2_3000; break;
+      case 4096: data = &L2_4096; break;
+      case 5000: data = &L2_5000; break;
+      default:   fprintf(stderr, "L2 - %ld", size); exit(EXIT_FAILURE);
       }
       field = L2_FIELD;
       offset = sizeof(L2_FIELD);
     }
     else if (NULL != (aux = strstr(fnames[i], DP_FILE_PREFIX))) {
-      int size = strtol(aux + sizeof(DP_FILE_PREFIX), NULL, 10);
+      size = strtol(aux + sizeof(DP_FILE_PREFIX), NULL, 10);
       while (fgets(ln, sizeof(ln), f)) {
         if (NULL != (aux = strstr(ln, DP_FIELD2))) {
           switch (size) {
           case 64:   data = &AVX_64;   break;
           case 100:  data = &AVX_100;  break;
           case 128:  data = &AVX_128;  break;
+          case 1000: data = &AVX_1000; break;
+          case 1024: data = &AVX_1024; break;
           case 2000: data = &AVX_2000; break;
           case 2048: data = &AVX_2048; break;
-          default:   exit(EXIT_FAILURE);
+          case 3000: data = &AVX_3000; break;
+          case 4096: data = &AVX_4096; break;
+          case 5000: data = &AVX_5000; break;
+          default:   fprintf(stderr, "AVX - %ld", size); exit(EXIT_FAILURE);
           }
           data->d[data->n] = strtod(aux + sizeof(DP_FIELD2), NULL);
           ++data->n;
@@ -147,9 +166,14 @@ int main(void)
           case 64:   data = &DP_64;   break;
           case 100:  data = &DP_100;  break;
           case 128:  data = &DP_128;  break;
+          case 1000: data = &DP_1000; break;
+          case 1024: data = &DP_1024; break;
           case 2000: data = &DP_2000; break;
           case 2048: data = &DP_2048; break;
-          default:   exit(EXIT_FAILURE);
+          case 3000: data = &DP_3000; break;
+          case 4096: data = &DP_4096; break;
+          case 5000: data = &DP_5000; break;
+          default:   fprintf(stderr, "DP - %ld", size); exit(EXIT_FAILURE);
           }
           data->d[data->n] = strtod(aux + sizeof(DP_FIELD1), NULL);
           ++data->n;
@@ -159,73 +183,147 @@ int main(void)
           case 64:   data = &DP_64;   break;
           case 100:  data = &DP_100;  break;
           case 128:  data = &DP_128;  break;
+          case 1000: data = &DP_1000; break;
+          case 1024: data = &DP_1024; break;
           case 2000: data = &DP_2000; break;
           case 2048: data = &DP_2048; break;
-          default:   exit(EXIT_FAILURE);
+          case 3000: data = &DP_3000; break;
+          case 4096: data = &DP_4096; break;
+          case 5000: data = &DP_5000; break;
+          default:   fprintf(stderr, "DP - %ld", size); exit(EXIT_FAILURE);
           }
           aux += sizeof(RUNTIME_FIELD);
-          data->runtime = strtod(aux, NULL);
+          if (OPTIMIZED == tag)
+            data->runtime_otimiz = strtod(aux, NULL);
+          else
+            data->runtime_normal = strtod(aux, NULL);
+        }
+        else if (NULL != (aux = strstr(ln, "MatRowVetOtimiz")) || NULL != (aux = strstr(ln , "MatMatRowOtimiz"))) {
+          tag = OPTIMIZED;
+        }
+        else if (NULL != (aux = strstr(ln, "MatRowVet")) || NULL != (aux = strstr(ln , "MatMatRow"))) {
+          tag = NORMAL;
         }
       }
-      data->runtime /= data->n;
+      data->runtime_otimiz /= data->n;
+      data->runtime_normal /= data->n;
       continue; /* EARLY CONTINUE */
     }
-#if 0
-    else {
-      fprintf(stderr, "Arquivo inesperado: %s\n", fnames[i]);
-      exit(EXIT_FAILURE);
-    }
-#endif
 
     while (fgets(ln, sizeof(ln), f)) {
       if (NULL != (aux = strstr(ln, RUNTIME_FIELD))) {
         aux += sizeof(RUNTIME_FIELD);
-        data->runtime = strtod(aux, NULL);
+        if (OPTIMIZED == tag)
+          data->runtime_otimiz = strtod(aux, NULL);
+        else
+          data->runtime_normal = strtod(aux, NULL);
       }
       else if (NULL != (aux = strstr(ln, field))) {
         data->d[data->n] = strtod(aux + offset, NULL);
         ++data->n;
       }
+      else if (NULL != (aux = strstr(ln, "MatRowVetOtimiz")) || NULL != (aux = strstr(ln , "MatMatRowOtimiz"))) {
+        tag = OPTIMIZED;
+      }
+      else if (NULL != (aux = strstr(ln, "MatRowVet")) || NULL != (aux = strstr(ln , "MatMatRow"))) {
+        tag = NORMAL;
+      }
     }
-    data->runtime /= data->n;
+    data->runtime_otimiz /= data->n;
+    data->runtime_normal /= data->n;
   }
 
-  puts("#N\t\t\tRUNTIME\n\tL3\t   L2CACHE    FLOPS_DP   FLOPS_AVX");
+  puts("#N\t\t\tRUNTIME OTIMIZADO\n\tL3\t   L2CACHE    FLOPS_DP   FLOPS_AVX");
   printf(
       "64\t%-10g %-10g %-10g %-10g\n"
       "100\t%-10g %-10g %-10g %-10g\n"
       "128\t%-10g %-10g %-10g %-10g\n"
+      "1000\t%-10g %-10g %-10g %-10g\n"
+      "1024\t%-10g %-10g %-10g %-10g\n"
       "2000\t%-10g %-10g %-10g %-10g\n"
-      "2048\t%-10g %-10g %-10g %-10g\n\n",
-      L3_64.runtime, L2_64.runtime, DP_64.runtime, AVX_64.runtime,
-      L3_100.runtime, L2_100.runtime, DP_100.runtime, AVX_100.runtime,
-      L3_128.runtime, L2_128.runtime, DP_128.runtime, AVX_128.runtime,
-      L3_2000.runtime, L2_2000.runtime, DP_2000.runtime, AVX_2000.runtime,
-      L3_2048.runtime, L2_2048.runtime, DP_2048.runtime, AVX_2048.runtime);
+      "2048\t%-10g %-10g %-10g %-10g\n"
+      "3000\t%-10g %-10g %-10g %-10g\n"
+      "4096\t%-10g %-10g %-10g %-10g\n"
+      "5000\t%-10g %-10g %-10g %-10g\n\n",
+      L3_64.runtime_otimiz, L2_64.runtime_otimiz, DP_64.runtime_otimiz, AVX_64.runtime_otimiz,
+      L3_100.runtime_otimiz, L2_100.runtime_otimiz, DP_100.runtime_otimiz, AVX_100.runtime_otimiz,
+      L3_128.runtime_otimiz, L2_128.runtime_otimiz, DP_128.runtime_otimiz, AVX_128.runtime_otimiz,
+      L3_1000.runtime_otimiz, L2_1000.runtime_otimiz, DP_1000.runtime_otimiz, AVX_1000.runtime_otimiz,
+      L3_1024.runtime_otimiz, L2_1024.runtime_otimiz, DP_1024.runtime_otimiz, AVX_1024.runtime_otimiz,
+      L3_2000.runtime_otimiz, L2_2000.runtime_otimiz, DP_2000.runtime_otimiz, AVX_2000.runtime_otimiz,
+      L3_2048.runtime_otimiz, L2_2048.runtime_otimiz, DP_2048.runtime_otimiz, AVX_2048.runtime_otimiz,
+      L3_3000.runtime_otimiz, L2_3000.runtime_otimiz, DP_3000.runtime_otimiz, AVX_3000.runtime_otimiz,
+      L3_4096.runtime_otimiz, L2_4096.runtime_otimiz, DP_4096.runtime_otimiz, AVX_4096.runtime_otimiz,
+      L3_5000.runtime_otimiz, L2_5000.runtime_otimiz, DP_5000.runtime_otimiz, AVX_5000.runtime_otimiz);
 
-  puts("#N\t\t\tL3\n\tMatPtrVet MatRowVet MatMatPtr MatMatRow");
-  printf("64\t");   printTable(&L3_64);   putchar('\n');
-  printf("100\t");  printTable(&L3_100);  putchar('\n');
-  printf("128\t");  printTable(&L3_128);  putchar('\n');
-  printf("2000\t"); printTable(&L3_2000); putchar('\n');
-  printf("2048\t"); printTable(&L3_2048); putchar('\n');
-  putchar('\n');
+  puts("#N\t\t\tRUNTIME NORMAL\n\tL3\t   L2CACHE    FLOPS_DP   FLOPS_AVX");
+  printf(
+      "64\t%-10g %-10g %-10g %-10g\n"
+      "100\t%-10g %-10g %-10g %-10g\n"
+      "128\t%-10g %-10g %-10g %-10g\n"
+      "1000\t%-10g %-10g %-10g %-10g\n"
+      "1024\t%-10g %-10g %-10g %-10g\n"
+      "2000\t%-10g %-10g %-10g %-10g\n"
+      "2048\t%-10g %-10g %-10g %-10g\n"
+      "3000\t%-10g %-10g %-10g %-10g\n"
+      "4096\t%-10g %-10g %-10g %-10g\n"
+      "5000\t%-10g %-10g %-10g %-10g\n\n",
+      L3_64.runtime_normal, L2_64.runtime_normal, DP_64.runtime_normal, AVX_64.runtime_normal,
+      L3_100.runtime_normal, L2_100.runtime_normal, DP_100.runtime_normal, AVX_100.runtime_normal,
+      L3_128.runtime_normal, L2_128.runtime_normal, DP_128.runtime_normal, AVX_128.runtime_normal,
+      L3_1000.runtime_normal, L2_1000.runtime_normal, DP_1000.runtime_normal, AVX_1000.runtime_normal,
+      L3_1024.runtime_normal, L2_1024.runtime_normal, DP_1024.runtime_normal, AVX_1024.runtime_normal,
+      L3_2000.runtime_normal, L2_2000.runtime_normal, DP_2000.runtime_normal, AVX_2000.runtime_normal,
+      L3_2048.runtime_normal, L2_2048.runtime_normal, DP_2048.runtime_normal, AVX_2048.runtime_normal,
+      L3_3000.runtime_normal, L2_3000.runtime_normal, DP_3000.runtime_normal, AVX_3000.runtime_normal,
+      L3_4096.runtime_normal, L2_4096.runtime_normal, DP_4096.runtime_normal, AVX_4096.runtime_normal,
+      L3_5000.runtime_normal, L2_5000.runtime_normal, DP_5000.runtime_normal, AVX_5000.runtime_normal);
 
-  puts("#N\t\t\tL2CACHE\n\tMatPtrVet MatRowVet MatMatPtr MatMatRow");
+  puts("#N\t\t\tL2");
+  puts("\t     MatRowVet             MatMatRow");
+  puts("\t Com        Sem        Com        Sem");
   printf("64\t");   printTable(&L2_64);   putchar('\n');
   printf("100\t");  printTable(&L2_100);  putchar('\n');
   printf("128\t");  printTable(&L2_128);  putchar('\n');
+  printf("1000\t"); printTable(&L2_1000); putchar('\n');
+  printf("1024\t"); printTable(&L2_1024); putchar('\n');
   printf("2000\t"); printTable(&L2_2000); putchar('\n');
   printf("2048\t"); printTable(&L2_2048); putchar('\n');
+  printf("3000\t"); printTable(&L2_3000); putchar('\n');
+  printf("4096\t"); printTable(&L2_4096); putchar('\n');
+  printf("5000\t"); printTable(&L2_5000); putchar('\n');
   putchar('\n');
 
-  printf("#N\t\t\tFLOPS_DP\t\t\t\t\tFLOPS_AVX\n\tMatPtrVet MatRowVet MatMatPtr MatMatRow\t\t");
-  puts("MatPtrVet MatRowVet MatMatPtr MatMatRow");
+  puts("#N\t\t\tL3");
+  puts("\t     MatRowVet             MatMatRow");
+  puts("\t Com        Sem        Com        Sem");
+  printf("64\t");   printTable(&L3_64);   putchar('\n');
+  printf("100\t");  printTable(&L3_100);  putchar('\n');
+  printf("128\t");  printTable(&L3_128);  putchar('\n');
+  printf("1000\t"); printTable(&L3_1000); putchar('\n');
+  printf("1024\t"); printTable(&L3_1024); putchar('\n');
+  printf("2000\t"); printTable(&L3_2000); putchar('\n');
+  printf("2048\t"); printTable(&L3_2048); putchar('\n');
+  printf("3000\t"); printTable(&L3_3000); putchar('\n');
+  printf("4096\t"); printTable(&L3_4096); putchar('\n');
+  printf("5000\t"); printTable(&L3_5000); putchar('\n');
+  putchar('\n');
+
+  printf("#N\t\t\tFLOPS_DP\t\t\t\t\tFLOPS_AVX\n");
+  printf("\t     MatRowVet             MatMatRow");
+  printf("\t\t   MatRowVet             MatMatRow\n");
+  printf("\t Com        Sem        Com        Sem");
+  printf("\t\tCom        Sem        Com        Sem\n");
   printf("64\t");   printTable(&DP_64);   putchar('\t'); printTable(&AVX_64);   putchar('\n');
   printf("100\t");  printTable(&DP_100);  putchar('\t'); printTable(&AVX_100);  putchar('\n');
   printf("128\t");  printTable(&DP_128);  putchar('\t'); printTable(&AVX_128);  putchar('\n');
+  printf("1000\t");  printTable(&DP_1000);  putchar('\t'); printTable(&AVX_1000);  putchar('\n');
+  printf("1024\t");  printTable(&DP_1024);  putchar('\t'); printTable(&AVX_1024);  putchar('\n');
   printf("2000\t"); printTable(&DP_2000); putchar('\t'); printTable(&AVX_2000); putchar('\n');
   printf("2048\t"); printTable(&DP_2048); putchar('\t'); printTable(&AVX_2048); putchar('\n');
+  printf("3000\t"); printTable(&DP_3000); putchar('\t'); printTable(&AVX_3000); putchar('\n');
+  printf("4096\t"); printTable(&DP_4096); putchar('\t'); printTable(&AVX_4096); putchar('\n');
+  printf("5000\t"); printTable(&DP_5000); putchar('\t'); printTable(&AVX_5000); putchar('\n');
   putchar('\n');
 
   return EXIT_SUCCESS;
