293e43464c866ca5527e3d8afa13e062d634f625
Luan-MB <luanbernardt@hotmail.com>
2021-08-12 13:27:15 -0300

    Liwkid

--- Trab2/matrixInv.c
+++ Trab2/matrixInv.c
@@ -5,7 +5,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <likwid.h>
 #include "matrixLib.h"
 
 
@@ -14,10 +14,12 @@ int main (int argc, char **argv) {
     t_sist *SL;
     FILE *f_out = stdout;
 
+    LIKWID_MARKER_INIT;   
     while (!feof(stdin))
     {
+
         SL = SL_leitura();
-        if (!SL) return EXIT_SUCCESS;
+        if (!SL) break;
         
         double *B = SL_alocaMatrix(1, SL->n);
         if (!B) return EXIT_FAILURE;
@@ -27,19 +29,26 @@ int main (int argc, char **argv) {
 
         for (int i=0; i<SL->m; ++i) {
             if (SL_interpolacao(SL, i, B)) return EXIT_FAILURE;
-            
+		
             // separa SL->Int em LU
-            if (SL_triangulariza(SL, SL->Int, B)) return EXIT_FAILURE;
-            SL_substituicao(SL, B, pol);
+            LIKWID_MARKER_START("Triangulariza");
+	    if (SL_triangulariza(SL, SL->Int, B)) return EXIT_FAILURE;
+            LIKWID_MARKER_STOP("Triangulariza");
+	    SL_substituicao(SL, B, pol);
 
             SL_printMatrix(f_out, pol, SL->n, 1);
-
+	    
+	    LIKWID_MARKER_START("AjusteDeCurvas");
             if (SL_ajusteDeCurvas(SL, i, B)) return EXIT_FAILURE;
+	    LIKWID_MARKER_STOP("AjusteDeCurvas");
 
             // separa SL->Ajc em LU
-            if (SL_triangulariza(SL, SL->Ajc, B)) return EXIT_FAILURE;
-            SL_substituicao(SL, B, pol);
+            LIKWID_MARKER_START("TriangularizaOtimiz");
+	    if (SL_triangulariza_otimiz(SL, SL->Ajc, B)) return EXIT_FAILURE;
+            LIKWID_MARKER_STOP("TriangularizaOtimiz");
 
+	    SL_substituicao(SL, B, pol);
+		
             SL_printMatrix(f_out, pol, SL->n, 1);
         }
 #if 0
@@ -51,8 +60,9 @@ int main (int argc, char **argv) {
         free(B);
         SL_libera(SL);
     }
-
+    
+    LIKWID_MARKER_CLOSE;
     if (f_out != stdout) fclose(f_out);
-
+    
     return EXIT_SUCCESS;
 }
